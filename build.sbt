2ca0490ecf5427ae04c744edf0f0469a112d90a1
saurahritu <firstrankforall@gmail.com>
2021-10-13 12:58:52 +0530

    init

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "SparkDemo"
+
+version := "0.1"
+
+scalaVersion := "2.12.0"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-core
+libraryDependencies += "org.apache.spark" %% "spark-core" % "3.1.2"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-sql
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.1.2" % "provided"
---
+++ src/main/scala/RDD_Demo.scala
@@ -0,0 +1,26 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+object RDD_Demo {
+  def main(args:Array[String]): Unit ={
+    val conf = new SparkConf().setMaster("local").setAppName("RDD_Demo")
+    val sc = new SparkContext(conf)
+    sc.setLogLevel("WARN")
+
+    // creating rdd1
+    val rdd1 = sc.parallelize(Array(1,2,3,4,5))
+    println("elements of rdd1: ")
+    rdd1.foreach(x => print(x + ","))
+    println()
+
+    // creating rdd2
+    val rdd2 = sc.parallelize(Array(6,7,8,9,10))
+    println("elements of rdd2: ")
+    rdd1.foreach(x => print(x + ","))
+    println()
+
+    val rdd3 = rdd1.union(rdd2)
+    println("elements of rdd3: ")
+    rdd3.foreach(x=>print(x + ","))
+    println()
+  }
+}
