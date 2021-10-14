a3b267e173c92389be931c644986c6f2dc565089
saurabhritu <firstrankforall@gmail.com>
2021-10-14 21:42:40 +0530

    modified to sp-submit

--- build.sbt
+++ build.sbt
@@ -2,10 +2,12 @@ name := "SparkDemo"
 
 version := "0.1"
 
-scalaVersion := "2.12.0"
+scalaVersion := "2.12.10"
 
-// https://mvnrepository.com/artifact/org.apache.spark/spark-core
-libraryDependencies += "org.apache.spark" %% "spark-core" % "3.1.2"
 
-// https://mvnrepository.com/artifact/org.apache.spark/spark-sql
-libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.1.2" % "provided"
+libraryDependencies ++= Seq(
+  // https://mvnrepository.com/artifact/org.apache.spark/spark-core
+  "org.apache.spark" %% "spark-core" % "3.1.2",
+  // https://mvnrepository.com/artifact/org.apache.spark/spark-sql
+  "org.apache.spark" %% "spark-sql" % "3.1.2"
+)
--- src/main/scala/RDD_Demo.scala
+++ src/main/scala/RDD_Demo.scala
@@ -1,4 +1,5 @@
 import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 object RDD_Demo {
   def main(args:Array[String]): Unit ={
@@ -7,20 +8,28 @@ object RDD_Demo {
     sc.setLogLevel("WARN")
 
     // creating rdd1
-    val rdd1 = sc.parallelize(Array(1,2,3,4,5))
-    println("elements of rdd1: ")
-    rdd1.foreach(x => print(x + ","))
-    println()
+//    val rdd1 = sc.parallelize(Array(1,2,3,4,5))
+//    println("elements of rdd1: ")
+//    rdd1.foreach(x => print(x + ","))
+//    println()
+//
+//    // creating rdd2
+//    val rdd2 = sc.parallelize(Array(6,7,8,9,10))
+//    println("elements of rdd2: ")
+//    rdd1.foreach(x => print(x + ","))
+//    println()
+//
+//    val rdd3 = rdd1.union(rdd2)
+//    println("elements of rdd3: ")
+//    rdd3.foreach(x=>print(x + ","))
+//    println()
 
-    // creating rdd2
-    val rdd2 = sc.parallelize(Array(6,7,8,9,10))
-    println("elements of rdd2: ")
-    rdd1.foreach(x => print(x + ","))
-    println()
+    val text_file = sc.textFile("/home/saurabh/Desktop/Spafka_RW/Read/SR_To_Do-list")
+    val rd_text = text_file.flatMap(_.split(" "))
+    val wordCounts = rd_text.map((_, 1)).reduceByKey(_ + _)
 
-    val rdd3 = rdd1.union(rdd2)
-    println("elements of rdd3: ")
-    rdd3.foreach(x=>print(x + ","))
-    println()
+    // Warning: always change the file name before running this application, otherwise it return with exit code 1 instead of 0.
+    wordCounts.saveAsTextFile("/home/saurabh/Desktop/Spafka_RW/Write/SR_To_Do-list_1")
+
   }
 }
