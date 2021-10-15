fc50920d25daed8ffdf4406b3265bf5dbfab7a4f
saurabhritu <firstrankforall@gmail.com>
2021-10-15 20:56:43 +0530

    word count spark-submit program addition

--- src/main/scala/RDD_Demo.scala
+++ src/main/scala/RDD_Demo.scala
@@ -1,34 +1,26 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
 object RDD_Demo {
   def main(args:Array[String]): Unit ={
     val conf = new SparkConf().setMaster("local").setAppName("RDD_Demo")
     val sc = new SparkContext(conf)
     sc.setLogLevel("WARN")
 
-    // creating rdd1
-//    val rdd1 = sc.parallelize(Array(1,2,3,4,5))
-//    println("elements of rdd1: ")
-//    rdd1.foreach(x => print(x + ","))
-//    println()
-//
-//    // creating rdd2
-//    val rdd2 = sc.parallelize(Array(6,7,8,9,10))
-//    println("elements of rdd2: ")
-//    rdd1.foreach(x => print(x + ","))
-//    println()
-//
-//    val rdd3 = rdd1.union(rdd2)
-//    println("elements of rdd3: ")
-//    rdd3.foreach(x=>print(x + ","))
-//    println()
-
+//    val text_file = sc.textFile("/home/saurabh/Desktop/Spafka_RW/Read/SR_To_Do-list")
     val text_file = sc.textFile(args(0))
+    println("Input Source: " + args(0))
     val rd_text = text_file.flatMap(_.split(" "))
     val wordCounts = rd_text.map((_, 1)).reduceByKey(_ + _)
-
+//    wordCounts.saveAsTextFile("/home/saurabh/Desktop/Spafka_RW/Write/SR_To_Do-list_1")
     wordCounts.saveAsTextFile(args(1))
+    println("Output Source: " + args(1))
 
   }
 }
+
+
+//spark-submit --class RDD_Demo target/scala-2.12/sparkdemo_2.12-1.0.jar local output /home/saurabh/Desktop/Spafka_RW/Read/SR_To_Do-list.txt
+//spark-submit --class RDD_Demo target/scala-2.12/sparkdemo_2.12-1.0.jar /home/saurabh/Desktop/Spafka_RW/Read/SR_To_Do-list.txt /home/saurabh/Desktop/Spafka_RW/Write/SR_To_Do-list_1
+
